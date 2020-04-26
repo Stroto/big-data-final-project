b36756fb83dd9ad0dd3bd33f320a8f1b092a57b9
Joshua <jp3742@columbia.edu>
2020-04-25 23:02:30 -0400

    Packaged Data

--- song_info_scrapper.py
+++ song_info_scrapper.py
@@ -43,9 +43,11 @@ def getSongInfo(song: tuple, i):
             if artists[count].lower() in hit['result']['primary_artist']['name'].lower():
                 song_page = hit['result']["url"]
                 song_api_path = hit['result']['api_path']
+                song_artist_page = hit['result']["primary_artist"]['url']                
                 
                 producers, view_count = getProducers_and_ViewCount(song_api_path)
                 lyrics = scrapeLyrics(song_page)
+                popular_songs = getPopularSongs(artists[count])
                 
                 print(f"COUNT: {i}\nSong: {song[0]}, Artists: {song[1]}, Rank: {song[2]}, Year: {song[3]}")
                 print(f"", producers, "\n--------")
@@ -81,6 +83,40 @@ def scrapeLyrics(url):
  
     return lyrics
 
+def re_strip(string):
+    new_string = re.sub(r'^(\u200b)+', "", string)
+    new_string = re.sub(r'(\u200b)+$', "", new_string)
+
+    return new_string
+
+def getPopularSongs(artist):
+    
+    
+    base_url = 'https://api.genius.com'
+    headers = {'Authorization': 'Bearer ' + CLIENT_ACCESS_TOKEN}
+    search_url = base_url + '/search'
+    data = {'q': artist}
+    json_res = requests.get(search_url, data=data, headers=headers).json()
+    
+    url = ""
+    
+    for hit in json_res['response']['hits']:
+        if artist.lower() in hit['result']['primary_artist']['name'].lower():
+            url = hit['result']['primary_artist']['url']
+            break
+        
+    res = scrapper.getResponseSoup(url)
+    
+    popular_songs_html_grid = res.find_all("div", class_ = "mini_card-title_and_subtitle")
+    popular_songs = [(grid.find("div", class_="mini_card-title").text.strip(), grid.find("div", class_="mini_card-subtitle").text.strip()) for grid in popular_songs_html_grid]
+    
+    popular_songs = [(re_strip(song[0]), re.split(r'\s*[,&)]\s*', song[1])) for song in popular_songs]
+    popular_songs = [(song[0], song[1]) for song in popular_songs if "Remix" not in song[0]]
+    
+    print(popular_songs)   
+        
+        
+        
 def main():
     
     url = "https://www.billboard.com/charts/year-end/2019/hot-100-songs"
